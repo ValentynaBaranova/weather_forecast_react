74232f7724049973a8c3964960d32ae0ab438de0
Valentyna Baranova <valentynabaranova5@gmail.com>
2022-08-15 15:20:04 +0300

    Added Githab rep URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <h1> Weather</h1>
+      <footer>
+        Created by
+        <a href="https://github.com/ValentynaBaranova/weather_forecast_react">
+          {" "}
+          Valentyna Baranova
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
